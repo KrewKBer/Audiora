acb421f8d3aeec5176b9f7d01328976b30e7ec80
Augustinas Pletkus <augustinas.pletkus@mif.stud.vu.lt>
2025-10-01 20:41:59 +0300

    now supports new songs.json file

--- Models/Song.cs
+++ Models/Song.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
 namespace Audiora.Models
 {
     public class Song
@@ -6,9 +9,25 @@ namespace Audiora.Models
         public string? Title { get; set; }
         public string? Artist { get; set; }
         public string? Album { get; set; }
-        public string? PrimaryGenre { get; set; }
-        public int Year { get; set; }
-        public int Duration { get; set; }
-        public string? Country { get; set; }
+
+        [JsonPropertyName("primaryGenre")]
+        public string? Genre { get; set; }
+        public List<string>? SubGenres { get; set; }
+        public string? Duration { get; set; }
+        public int ReleaseYear { get; set; }
+        public List<string>? Mood { get; set; }
+        public string? Tempo { get; set; }
+        public double Energy { get; set; }
+        public double Danceability { get; set; }
+        public double Instrumentalness { get; set; }
+        public double Valence { get; set; }
+        public int Popularity { get; set; }
+        public string? Language { get; set; }
+        public List<string>? Themes { get; set; }
+        public List<string>? SimilarArtists { get; set; }
+        public List<string>? Tags { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? AudioUrl { get; set; }
+        public string? SpotifyId { get; set; }
     }
 }
