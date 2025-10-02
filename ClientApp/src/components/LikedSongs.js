5d4f96f0db83c1ac4bf46652d55933c4c2dfae5a
Edvinas Ryliškis <edvinas.ryliskis@mif.stud.vu.lt>
2025-10-02 13:24:50 +0300

    Fixing liked songs page, adding LikedSongs.css

--- ClientApp/src/components/LikedSongs.js
+++ ClientApp/src/components/LikedSongs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../custom.css'; 
+import './LikedSongs.css'; 
 
 export class LikedSongs extends Component {
   static displayName = LikedSongs.name;
