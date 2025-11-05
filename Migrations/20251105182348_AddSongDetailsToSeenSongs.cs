a0d09131d1059700cc2e69330668dc2d31799027
augustinas <augustinas.pletkus@mif.stud.vu.lt>
2025-11-05 20:39:47 +0200

    migration stuff

---
+++ Migrations/20251105182348_AddSongDetailsToSeenSongs.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Audiora.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSongDetailsToSeenSongs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AlbumImageUrl",
+                table: "SeenSongs",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Artist",
+                table: "SeenSongs",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "SeenSongs",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlbumImageUrl",
+                table: "SeenSongs");
+
+            migrationBuilder.DropColumn(
+                name: "Artist",
+                table: "SeenSongs");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "SeenSongs");
+        }
+    }
+}
