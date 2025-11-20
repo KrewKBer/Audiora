1b4a76c01a7b6c4f5037a95a5a3f639f10cd2e4c
Edvinas Ryliškis <edvinas.ryliskis@mif.stud.vu.lt>
2025-11-20 14:36:41 +0200

    feat: add generic Map method, refactor exceptions, and fix build config

--- Exceptions/SpotifyApiException.cs
+++ Exceptions/SpotifyApiException.cs
@@ -2,15 +2,6 @@ namespace Audiora.Exceptions;
 
 public class SpotifyApiException : Exception
 {
-    public SpotifyApiException()
-    {
-    }
-
-    public SpotifyApiException(string message)
-        : base(message)
-    {
-    }
-
     public SpotifyApiException(string message, Exception inner)
         : base(message, inner)
     {
